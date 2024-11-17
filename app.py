701d058ad4eab2fdf68eacb9d1c15a23bfeb65cd
Dixing Xu <i@dex.moe>
2024-11-17 08:25:36 +0800

    feat: Init.

---
+++ app.py
@@ -0,0 +1,26 @@
+import os
+from dstack_sdk import AsyncTappdClient, DeriveKeyResponse, TdxQuoteResponse
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "The World! Call /derivekey or /tdxquote"}
+
+@app.get("/derivekey")
+async def derivekey():
+    client = AsyncTappdClient()
+    deriveKey = await client.derive_key('/', 'test')
+    assert isinstance(deriveKey, DeriveKeyResponse)
+    asBytes = deriveKey.toBytes()
+    assert isinstance(asBytes, bytes)
+    limitedSize = deriveKey.toBytes(32)
+    return {"deriveKey": asBytes.hex(), "derive_32bytes": limitedSize.hex()}
+    
+@app.get("/tdxquote")
+async def tdxquote():
+    client = AsyncTappdClient()
+    tdxQuote = await client.tdx_quote('test')
+    assert isinstance(tdxQuote, TdxQuoteResponse)
+    return {"tdxQuote": tdxQuote}
