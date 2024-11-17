8abc71c0d5930cea8548d624c0ea411eccef8efd
Dixing Xu <i@dex.moe>
2024-11-17 10:10:06 +0800

    feat: Add frontend

---
+++ DataDee/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app>
+    <v-main>
+      <MarketPlace />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+import MarketPlace from '@/components/MarketPlace.vue'
+</script>
